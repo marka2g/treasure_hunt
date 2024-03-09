76adaee83e7e2ef8878985ab182cafc818e34036
Mark Sadegi <mark.sadegi@gmail.com>
2024-03-09 14:50:04 -0800

    add Game test

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,14 +7,15 @@ class Game < ApplicationRecord
 
   scope :ranked, -> { where.not(winning_distance: nil).order(winning_distance: :asc) }
   
-
-  def to_s
-    "The game board:\n#{@board}"
-  end
-
   def move(move_coords)
     calculate_current_position(move_coords)
-    winning_move?(move_coords) ? win(move_coords) : keep_playing(move_coords)
+    dist = distance_from_treasure()
+    winning_move?(dist) ? win(dist) : keep_playing(dist)
+  end
+
+  # public to stub in test
+  def x_y_ary(size)
+    [rand(0..19), rand(0..19)]
   end
 
 private
@@ -25,35 +26,29 @@ private
     self.current_position = [0, 0]
   end
 
-  # stubbed for test
-  def x_y_ary(size)
-    [rand(0..19), rand(0..19)]
-  end
-
   def build_board
     (1..self.board_size.to_i*self.board_size.to_i).each_slice(self.board_size.to_i).to_a
   end
 
   def distance_from_treasure
-    ((self.current_position[0] - self.treasure_x.to_i).abs + (self.current_position[1] - self.treasure_y.to_i).abs)  * 100
+    ((self.treasure_x.to_i - self.current_position[0]).abs + (self.treasure_y.to_i - self.current_position[1]).abs)  * 100
   end
 
-  def winning_move?(move_coords)
-    distance_from_treasure() < 1000
+  def winning_move?(dist_to_treasure)
+    dist_to_treasure < 1000
   end
 
   def calculate_current_position(coords)
     self.current_position = [self.current_position[0] += coords[0], self.current_position[1] += coords[1]]
   end
 
-  def win(move_coords)
-    dist = distance_from_treasure()
-    update({winning_distance: dist})
-    [true, "💎 The treasure is #{dist} meters away - you win! 💎"]
+  def win(dist_to_treasure)
+    update({winning_distance: dist_to_treasure})
+    [true, "💎 The treasure is #{dist_to_treasure} meters away - you win! 💎"]
   end
 
-  def keep_playing(move_coords)
-    [false, "🙅🏽‍♂️ Sorry, you are #{distance_from_treasure()} meters away. Try again. 🙅🏽‍♂️"]
+  def keep_playing(dist_to_treasure)
+    [false, "🙅🏽‍♂️ Sorry, you are #{dist_to_treasure} meters away. Try again. 🙅🏽‍♂️"]
   end
 
 end
