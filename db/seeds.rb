7ffde8a943c9a7b25f6dca00b9a2f8734d6ce1ca
Mark Sadegi <12756+marka2g@users.noreply.github.com>
2024-03-11 11:21:34 -0700

    Add basic pagination (#4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-[
-  ["blah@example.com", 100], 
-  ["meh@example.com", 200], 
-  ["foo@example.com", 300], 
-  ["bar@example.com", 400], 
-  ["hunter@example.com", nil]
-].each do |email, score|
-  user = User.where(email: email).first_or_initialize
+# to use, run: rails db:seed
+# to start fresh, run: `rails db:migrate:reset` and then `rails db:seed`
+test_player = User.create(email: "test-hunter@example.com")
+test_player.update!(
+  password: "password",
+  password_confirmation: "password"
+)
 
-  # update! here will create user without create() which will cause errors
+# pagination testing
+50.times do |i|
+  user = User.where(email: "hunter-#{i+1}@example.com").first_or_initialize
   user.update!(
     password: "password",
     password_confirmation: "password"
   )
+  Game.create(user: user, board_size: 20, winning_distance: %w( 0 100 200 300 400 500 600 700 800 900).sample.to_i)
 
-  if score
-    Game.create(user: user, board_size: 20, winning_distance: score)
-  end
+  puts "User with email: #{user.email} created"
 end
